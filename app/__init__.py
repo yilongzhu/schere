0c61346136030458ea9a7a8b7af3d89106c8fb32
Yilong Zhu <me@yilongzhu.com>
2018-10-21 02:05:50 -0400

    Add config and requirement files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 
 
-def create_app():
+def create_app(config_class=Config):
     application = Flask(__name__)
+    application.config.from_object(config_class)
 
     from app.proto import bp as proto_bp
     application.register_blueprint(proto_bp, url_prefix='/proto')
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = '2cf6f26d05281dd899b5fd80a648becfe9c1fa7f9a593437'
